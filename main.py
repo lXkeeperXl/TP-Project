51e495973efa1e869f86008b728e83cb088fe550
SvkKirill <ska.22@uni-dubna.ru>
2024-12-25 02:14:19 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,187 @@
+import sys
+import os
+import platform
+import datetime
+
+from modules import *
+from modules.app_functions import AppFunctions
+
+# IMPORT QT CORE
+# ///////////////////////////////////////////////////////////////
+from qt_core import *
+
+# SETTINGS
+# ///////////////////////////////////////////////////////////////
+_settings = QSettings()
+_settings.setValue("theme", "dark")  # Установка темы по умолчанию
+
+os.environ["QT_FONT_DPI"] = "96"
+
+# LOAD THEME
+# ///////////////////////////////////////////////////////////////
+def loadTheme():
+    theme = _settings.value("theme")
+    if theme == "dark":
+        app.setStyleSheet(qdarkstyle.load_stylesheet_pyside6())
+    elif theme == "light":
+        app.setStyleSheet("")  # Сброс стилей для светлой темы
+
+widgets = None
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        QMainWindow.__init__(self)
+
+        # SET AS GLOBAL WIDGETS
+        # ///////////////////////////////////////////////////////////////
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        global widgets
+        widgets = self.ui
+
+        # USE CUSTOM TITLE BAR | USE AS "False" FOR MAC OR LINUX
+        # ///////////////////////////////////////////////////////////////
+        Settings.ENABLE_CUSTOM_TITLE_BAR = True
+
+        # APP NAME
+        # ///////////////////////////////////////////////////////////////
+        title = "Modern Notes"
+        description = "Modern and convenient"
+
+        # APPLY TEXTS
+        self.setWindowTitle(title)
+        widgets.titleRightInfo.setText(description)
+
+        # TOGGLE MENU
+        # ///////////////////////////////////////////////////////////////
+        widgets.toggleButton.clicked.connect(lambda: UIFunctions.toggleMenu(self, True))
+
+        # SET UI DEFINITIONS
+        # ///////////////////////////////////////////////////////////////
+        UIFunctions.uiDefinitions(self)
+
+        # QTableWidget PARAMETERS
+        # ///////////////////////////////////////////////////////////////
+        widgets.tableWidget.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
+
+        # BUTTONS CLICK
+        # ///////////////////////////////////////////////////////////////
+
+        # LEFT MENUS
+        widgets.btn_home.clicked.connect(self.buttonClick)
+        widgets.btn_widgets.clicked.connect(self.buttonClick)
+        widgets.btn_new.clicked.connect(self.buttonClick)
+        widgets.btn_save.clicked.connect(self.buttonClick)
+
+        # EXTRA LEFT BOX
+        def openCloseLeftBox():
+            UIFunctions.toggleLeftBox(self, True)
+        widgets.toggleLeftBox.clicked.connect(openCloseLeftBox)
+        widgets.extraCloseColumnBtn.clicked.connect(openCloseLeftBox)
+
+        # EXTRA RIGHT BOX
+        def openCloseRightBox():
+            UIFunctions.toggleRightBox(self, True)
+        widgets.settingsTopBtn.clicked.connect(openCloseRightBox)
+
+        # SHOW APP
+        # ///////////////////////////////////////////////////////////////
+        self.show()
+
+        # SET CUSTOM THEME
+        # ///////////////////////////////////////////////////////////////
+        useCustomTheme = False
+        themeFile = "themes/pylight.qss"
+
+        # SET THEME AND HACKS
+        if useCustomTheme:
+            # LOAD AND APPLY STYLE
+            UIFunctions.theme(self, themeFile, True)
+
+            # SET HACKS
+            AppFunctions.setThemeHack(self)
+
+        # SET HOME PAGE AND SELECT MENU
+        # ///////////////////////////////////////////////////////////////
+        widgets.stackedWidget.setCurrentWidget(widgets.home)
+        widgets.btn_home.setStyleSheet(UIFunctions.selectMenu(widgets.btn_home.styleSheet()))
+
+        # Создание экземпляра AppFunctions
+        self.app_functions = AppFunctions()
+
+        # Загрузка задач при старте приложения
+        self.tasks = self.app_functions.load_tasks_from_file()
+
+        # Инициализация таймера для проверки уведомлений
+        self.notification_timer = QTimer(self)
+        self.notification_timer.timeout.connect(self.check_notifications)
+        self.notification_timer.start(60000)  # Проверяем каждую минуту (60000 мс)
+
+        # Примеры вызова функций (нужно адаптировать под ваш UI)
+        # self.ui.add_task_button.clicked.connect(self.add_task_clicked)
+        # self.ui.edit_task_button.clicked.connect(self.edit_task_clicked)
+        # self.ui.delete_task_button.clicked.connect(self.delete_task_clicked)
+        # self.ui.add_tag_button.clicked.connect(self.add_tag_clicked)
+        # self.ui.remove_tag_button.clicked.connect(self.remove_tag_clicked)
+        # self.ui.add_notification_button.clicked.connect(self.add_notification_clicked)
+        # self.ui.remove_notification_button.clicked.connect(self.remove_notification_clicked)
+
+    def check_notifications(self):
+        """Проверяет и отправляет уведомления."""
+        self.app_functions.check_and_send_notifications()
+
+    # BUTTONS CLICK
+    # ///////////////////////////////////////////////////////////////
+    def buttonClick(self):
+        # GET BUTTON CLICKED
+        btn = self.sender()
+        btnName = btn.objectName()
+
+        # SHOW HOME PAGE
+        if btnName == "btn_home":
+            widgets.stackedWidget.setCurrentWidget(widgets.home)
+            UIFunctions.resetStyle(self, btnName)
+            btn.setStyleSheet(UIFunctions.selectMenu(btn.styleSheet()))
+
+        # SHOW WIDGETS PAGE
+        if btnName == "btn_widgets":
+            widgets.stackedWidget.setCurrentWidget(widgets.widgets)
+            UIFunctions.resetStyle(self, btnName)
+            btn.setStyleSheet(UIFunctions.selectMenu(btn.styleSheet()))
+
+        # SHOW NEW PAGE
+        if btnName == "btn_new":
+            widgets.stackedWidget.setCurrentWidget(widgets.new_page) # SET PAGE
+            UIFunctions.resetStyle(self, btnName) # RESET ANOTHERS BUTTONS SELECTED
+            btn.setStyleSheet(UIFunctions.selectMenu(btn.styleSheet())) # SELECT MENU
+
+        if btnName == "btn_save":
+            print("Save BTN clicked!")
+
+        # PRINT BTN NAME
+        print(f'Button "{btnName}" pressed!')
+
+    # RESIZE EVENTS
+    # ///////////////////////////////////////////////////////////////
+    def resizeEvent(self, event):
+        # Update Size Grips
+        UIFunctions.resize_grips(self)
+
+    # MOUSE CLICK EVENTS
+    # ///////////////////////////////////////////////////////////////
+    def mousePressEvent(self, event):
+        # SET DRAG POS WINDOW
+        self.dragPos = event.globalPos()
+
+        # PRINT MOUSE EVENTS
+        if event.buttons() == Qt.LeftButton:
+            print('Mouse click: LEFT CLICK')
+        if event.buttons() == Qt.RightButton:
+            print('Mouse click: RIGHT CLICK')
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    app.setWindowIcon(QIcon("icon.png"))
+    loadTheme() # Загрузка темы
+    window = MainWindow()
+    sys.exit(app.exec_())
